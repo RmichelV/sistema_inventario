9abb390e31dd5931699d29638d44a89b993c685f
Michel <michel@192.168.1.6>
2025-10-25 10:54:30 -0400

    final

--- resources/js/pages/Sales/create.vue
+++ resources/js/pages/Sales/create.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { computed, watch } from 'vue';
 import { Head, useForm } from '@inertiajs/vue3';
 import { LoaderCircle, Trash2 } from 'lucide-vue-next';
 
